eb16bae511d574fdf9c8e2008adb7093169c41f3
onnyf <onnyf2003@gmail.com>
2025-01-17 14:23:34 +0100

    second commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,22 +15,24 @@ const Footer = () => {
                         <h4>Contact</h4>
                         <p>11 Broadway Lobby 4</p>
                         <p>New York, NY 10004</p>
+                        <p>(234) 08 98 243</p>
+                    </div>
                 </div>
                 <div className="footer-box"></div>
                 <div className="footer-box">
                     <div className='footer-box-child'>
                         <h4>Menu</h4>
-                        <p><a href="##">Home</a></p>
-                        <p><a href="##">Shop</a></p>
-                        <p><a href="##">About Us</a></p>
-                        <p><a href="##">Contact Us</a></p>
+                        <p><a href="/">Home</a></p>
+                        <p><a href="/">Shop</a></p>
+                        <p><a href="/">About Us</a></p>
+                        <p><a href="/">Contact Us</a></p>
                     </div>
                     <div className='footer-box-child4'>
                         <h4>Fast Links</h4>
-                        <p><a href="##">Cart</a></p>
-                        <p><a href="##">My account</a></p>
-                        <p><a href="##">Contact Us</a></p>
-                        <p><a href="##">Reviews</a></p>
+                        <p><a href="#cart">Cart</a></p>
+                        <p><a href="#account">My Account</a></p>
+                        <p><a href="#contact">Contact Us</a></p>
+                        <p><a href="#review">Reviews</a></p>
                     </div>
                 </div>
                 <div className="footer-box"></div>
@@ -44,9 +46,9 @@ const Footer = () => {
                 <div className='right'>
                     <div className="icons-container">
                         <div>
-                            <a href="##"><img src="./image/face 4.png" alt="" /></a>
-                            <a href="##"><img src="./image/insta 5.png" alt="" /></a>
-                            <a href="##"><img src="./image/lind 7.png" alt="" /></a>
+                            <a href=""><img src="./image/face 4.png" alt="" /></a>
+                            <a href=""><img src="./image/insta 5.png" alt="" /></a>
+                            <a href=""><img src="./image/lind 7.png" alt="" /></a>
                         </div>
                     </div>
                 </div>
